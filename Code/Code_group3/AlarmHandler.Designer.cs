abd3e97d94a6b6773674621b7520ad738cfb158f
weihrauch96 <kris-123@hotmail.no>
2023-09-08 14:32:15 +0200

    Updating monitoring screen form

--- Code/Code_group3/AlarmHandler.Designer.cs
+++ Code/Code_group3/AlarmHandler.Designer.cs
@@ -48,10 +48,10 @@
             // btnAlarmActive
             // 
             this.btnAlarmActive.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAlarmActive.Location = new System.Drawing.Point(24, 176);
-            this.btnAlarmActive.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAlarmActive.Location = new System.Drawing.Point(32, 217);
+            this.btnAlarmActive.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnAlarmActive.Name = "btnAlarmActive";
-            this.btnAlarmActive.Size = new System.Drawing.Size(123, 35);
+            this.btnAlarmActive.Size = new System.Drawing.Size(164, 43);
             this.btnAlarmActive.TabIndex = 0;
             this.btnAlarmActive.Text = "Away (Active)";
             this.btnAlarmActive.UseVisualStyleBackColor = true;
@@ -60,10 +60,10 @@
             // btnAlarmDeactive
             // 
             this.btnAlarmDeactive.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAlarmDeactive.Location = new System.Drawing.Point(24, 256);
-            this.btnAlarmDeactive.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAlarmDeactive.Location = new System.Drawing.Point(32, 315);
+            this.btnAlarmDeactive.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnAlarmDeactive.Name = "btnAlarmDeactive";
-            this.btnAlarmDeactive.Size = new System.Drawing.Size(123, 35);
+            this.btnAlarmDeactive.Size = new System.Drawing.Size(164, 43);
             this.btnAlarmDeactive.TabIndex = 1;
             this.btnAlarmDeactive.Text = "Home (Deactive)";
             this.btnAlarmDeactive.UseVisualStyleBackColor = true;
@@ -72,21 +72,21 @@
             // dgvAlarmList
             // 
             this.dgvAlarmList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvAlarmList.Location = new System.Drawing.Point(69, 37);
-            this.dgvAlarmList.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvAlarmList.Location = new System.Drawing.Point(92, 46);
+            this.dgvAlarmList.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dgvAlarmList.Name = "dgvAlarmList";
             this.dgvAlarmList.RowHeadersWidth = 51;
             this.dgvAlarmList.RowTemplate.Height = 24;
-            this.dgvAlarmList.Size = new System.Drawing.Size(370, 180);
+            this.dgvAlarmList.Size = new System.Drawing.Size(493, 222);
             this.dgvAlarmList.TabIndex = 2;
             // 
             // btnLog
             // 
             this.btnLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLog.Location = new System.Drawing.Point(132, 246);
-            this.btnLog.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLog.Location = new System.Drawing.Point(176, 303);
+            this.btnLog.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnLog.Name = "btnLog";
-            this.btnLog.Size = new System.Drawing.Size(80, 35);
+            this.btnLog.Size = new System.Drawing.Size(107, 43);
             this.btnLog.TabIndex = 4;
             this.btnLog.Text = "Log";
             this.btnLog.UseVisualStyleBackColor = true;
@@ -94,20 +94,20 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(292, 246);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Location = new System.Drawing.Point(389, 303);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(90, 35);
+            this.button1.Size = new System.Drawing.Size(120, 43);
             this.button1.TabIndex = 5;
             this.button1.Text = "Reset Alarm";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // btnHome
             // 
-            this.btnHome.Location = new System.Drawing.Point(877, 11);
-            this.btnHome.Margin = new System.Windows.Forms.Padding(2);
+            this.btnHome.Location = new System.Drawing.Point(12, 647);
+            this.btnHome.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnHome.Name = "btnHome";
-            this.btnHome.Size = new System.Drawing.Size(64, 20);
+            this.btnHome.Size = new System.Drawing.Size(85, 25);
             this.btnHome.TabIndex = 6;
             this.btnHome.Text = "Home";
             this.btnHome.UseVisualStyleBackColor = true;
@@ -118,19 +118,22 @@
             this.panel1.BackColor = System.Drawing.Color.MistyRose;
             this.panel1.Controls.Add(this.picBoxDeactive);
             this.panel1.Controls.Add(this.picBoxActive);
+            this.panel1.Controls.Add(this.btnHome);
             this.panel1.Controls.Add(this.btnAlarmActive);
             this.panel1.Controls.Add(this.btnAlarmDeactive);
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(198, 566);
+            this.panel1.Size = new System.Drawing.Size(264, 697);
             this.panel1.TabIndex = 7;
             // 
             // picBoxDeactive
             // 
             this.picBoxDeactive.Image = global::Code_group3.Properties.Resources._checked;
-            this.picBoxDeactive.Location = new System.Drawing.Point(152, 256);
+            this.picBoxDeactive.Location = new System.Drawing.Point(203, 315);
+            this.picBoxDeactive.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.picBoxDeactive.Name = "picBoxDeactive";
-            this.picBoxDeactive.Size = new System.Drawing.Size(43, 35);
+            this.picBoxDeactive.Size = new System.Drawing.Size(57, 43);
             this.picBoxDeactive.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picBoxDeactive.TabIndex = 9;
             this.picBoxDeactive.TabStop = false;
@@ -139,9 +142,10 @@
             // picBoxActive
             // 
             this.picBoxActive.Image = global::Code_group3.Properties.Resources.circle_button;
-            this.picBoxActive.Location = new System.Drawing.Point(152, 176);
+            this.picBoxActive.Location = new System.Drawing.Point(203, 217);
+            this.picBoxActive.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.picBoxActive.Name = "picBoxActive";
-            this.picBoxActive.Size = new System.Drawing.Size(43, 35);
+            this.picBoxActive.Size = new System.Drawing.Size(57, 43);
             this.picBoxActive.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picBoxActive.TabIndex = 9;
             this.picBoxActive.TabStop = false;
@@ -152,23 +156,24 @@
             this.AlarmList.Controls.Add(this.dgvAlarmList);
             this.AlarmList.Controls.Add(this.btnLog);
             this.AlarmList.Controls.Add(this.button1);
-            this.AlarmList.Location = new System.Drawing.Point(299, 97);
+            this.AlarmList.Location = new System.Drawing.Point(399, 119);
+            this.AlarmList.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.AlarmList.Name = "AlarmList";
-            this.AlarmList.Size = new System.Drawing.Size(508, 339);
+            this.AlarmList.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.AlarmList.Size = new System.Drawing.Size(677, 417);
             this.AlarmList.TabIndex = 8;
             this.AlarmList.TabStop = false;
             this.AlarmList.Text = "Alarm List";
             // 
             // AlarmHandler
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(952, 566);
+            this.ClientSize = new System.Drawing.Size(1269, 697);
             this.Controls.Add(this.AlarmList);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.btnHome);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "AlarmHandler";
             this.Text = "AlarmHandler";
             ((System.ComponentModel.ISupportInitialize)(this.dgvAlarmList)).EndInit();
--- Code/Code_group3/Monitoring.Designer.cs
+++ Code/Code_group3/Monitoring.Designer.cs
@@ -38,67 +38,69 @@
             this.btnOffOn = new System.Windows.Forms.Button();
             this.btnHome = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dgvM)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgvM
             // 
             this.dgvM.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvM.Location = new System.Drawing.Point(295, 80);
-            this.dgvM.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvM.Location = new System.Drawing.Point(479, 115);
+            this.dgvM.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dgvM.Name = "dgvM";
             this.dgvM.RowHeadersWidth = 51;
             this.dgvM.RowTemplate.Height = 24;
-            this.dgvM.Size = new System.Drawing.Size(256, 228);
+            this.dgvM.Size = new System.Drawing.Size(697, 312);
             this.dgvM.TabIndex = 0;
             // 
             // txtHeatStatus
             // 
             this.txtHeatStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtHeatStatus.Location = new System.Drawing.Point(9, 63);
-            this.txtHeatStatus.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtHeatStatus.Location = new System.Drawing.Point(13, 45);
+            this.txtHeatStatus.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtHeatStatus.Name = "txtHeatStatus";
-            this.txtHeatStatus.Size = new System.Drawing.Size(102, 23);
+            this.txtHeatStatus.Size = new System.Drawing.Size(144, 27);
             this.txtHeatStatus.TabIndex = 2;
             this.txtHeatStatus.Text = "Heat Status";
             // 
             // txtCoolingStatus
             // 
             this.txtCoolingStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCoolingStatus.Location = new System.Drawing.Point(124, 63);
-            this.txtCoolingStatus.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtCoolingStatus.Location = new System.Drawing.Point(190, 45);
+            this.txtCoolingStatus.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtCoolingStatus.Name = "txtCoolingStatus";
-            this.txtCoolingStatus.Size = new System.Drawing.Size(109, 23);
+            this.txtCoolingStatus.Size = new System.Drawing.Size(144, 27);
             this.txtCoolingStatus.TabIndex = 3;
             this.txtCoolingStatus.Text = "Cooling Status";
             // 
             // txtProcessValue
             // 
             this.txtProcessValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtProcessValue.Location = new System.Drawing.Point(124, 102);
-            this.txtProcessValue.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtProcessValue.Location = new System.Drawing.Point(190, 124);
+            this.txtProcessValue.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtProcessValue.Name = "txtProcessValue";
-            this.txtProcessValue.Size = new System.Drawing.Size(109, 23);
+            this.txtProcessValue.Size = new System.Drawing.Size(144, 27);
             this.txtProcessValue.TabIndex = 4;
             this.txtProcessValue.Text = "Process Value";
             // 
             // txtSetTempValue
             // 
             this.txtSetTempValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSetTempValue.Location = new System.Drawing.Point(9, 102);
-            this.txtSetTempValue.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtSetTempValue.Location = new System.Drawing.Point(13, 124);
+            this.txtSetTempValue.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtSetTempValue.Name = "txtSetTempValue";
-            this.txtSetTempValue.Size = new System.Drawing.Size(102, 23);
+            this.txtSetTempValue.Size = new System.Drawing.Size(144, 27);
             this.txtSetTempValue.TabIndex = 5;
             this.txtSetTempValue.Text = "Set Temp Value";
             // 
             // btnLightManual
             // 
             this.btnLightManual.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLightManual.Location = new System.Drawing.Point(16, 158);
-            this.btnLightManual.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLightManual.Location = new System.Drawing.Point(13, 192);
+            this.btnLightManual.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnLightManual.Name = "btnLightManual";
-            this.btnLightManual.Size = new System.Drawing.Size(93, 24);
+            this.btnLightManual.Size = new System.Drawing.Size(144, 30);
             this.btnLightManual.TabIndex = 6;
             this.btnLightManual.Text = "Light Manual";
             this.btnLightManual.UseVisualStyleBackColor = true;
@@ -106,10 +108,10 @@
             // btnLightAuto
             // 
             this.btnLightAuto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLightAuto.Location = new System.Drawing.Point(137, 158);
-            this.btnLightAuto.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLightAuto.Location = new System.Drawing.Point(190, 192);
+            this.btnLightAuto.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnLightAuto.Name = "btnLightAuto";
-            this.btnLightAuto.Size = new System.Drawing.Size(75, 24);
+            this.btnLightAuto.Size = new System.Drawing.Size(144, 30);
             this.btnLightAuto.TabIndex = 7;
             this.btnLightAuto.Text = "Light Auto";
             this.btnLightAuto.UseVisualStyleBackColor = true;
@@ -117,20 +119,20 @@
             // btnOffOn
             // 
             this.btnOffOn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnOffOn.Location = new System.Drawing.Point(18, 213);
-            this.btnOffOn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnOffOn.Location = new System.Drawing.Point(102, 273);
+            this.btnOffOn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnOffOn.Name = "btnOffOn";
-            this.btnOffOn.Size = new System.Drawing.Size(92, 24);
+            this.btnOffOn.Size = new System.Drawing.Size(133, 30);
             this.btnOffOn.TabIndex = 8;
             this.btnOffOn.Text = "Light Off/On";
             this.btnOffOn.UseVisualStyleBackColor = true;
             // 
             // btnHome
             // 
-            this.btnHome.Location = new System.Drawing.Point(514, 11);
-            this.btnHome.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnHome.Location = new System.Drawing.Point(13, 643);
+            this.btnHome.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnHome.Name = "btnHome";
-            this.btnHome.Size = new System.Drawing.Size(75, 19);
+            this.btnHome.Size = new System.Drawing.Size(100, 23);
             this.btnHome.TabIndex = 9;
             this.btnHome.Text = "Home";
             this.btnHome.UseVisualStyleBackColor = true;
@@ -140,33 +142,43 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(362, 57);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(745, 79);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(148, 20);
+            this.label1.Size = new System.Drawing.Size(183, 25);
             this.label1.TabIndex = 10;
             this.label1.Text = "Temperature logger";
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Controls.Add(this.txtHeatStatus);
+            this.panel1.Controls.Add(this.txtCoolingStatus);
+            this.panel1.Controls.Add(this.btnHome);
+            this.panel1.Controls.Add(this.txtSetTempValue);
+            this.panel1.Controls.Add(this.btnOffOn);
+            this.panel1.Controls.Add(this.txtProcessValue);
+            this.panel1.Controls.Add(this.btnLightAuto);
+            this.panel1.Controls.Add(this.btnLightManual);
+            this.panel1.Location = new System.Drawing.Point(-1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(368, 702);
+            this.panel1.TabIndex = 11;
+            // 
             // Monitoring
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1269, 697);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnHome);
-            this.Controls.Add(this.btnOffOn);
-            this.Controls.Add(this.btnLightAuto);
-            this.Controls.Add(this.btnLightManual);
-            this.Controls.Add(this.txtSetTempValue);
-            this.Controls.Add(this.txtProcessValue);
-            this.Controls.Add(this.txtCoolingStatus);
-            this.Controls.Add(this.txtHeatStatus);
             this.Controls.Add(this.dgvM);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Monitoring";
             this.Text = "Monitoring";
             ((System.ComponentModel.ISupportInitialize)(this.dgvM)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -184,5 +196,6 @@
         private System.Windows.Forms.Button btnOffOn;
         private System.Windows.Forms.Button btnHome;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
