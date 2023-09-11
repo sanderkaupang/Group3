a9feb5ffd3eeab407505620c4b20c06fb0d1592b
weihrauch96 <kris-123@hotmail.no>
2023-09-11 20:35:44 +0200

    Created con string to SQL and Azure
    Created a test form for testing tables from sql.

--- Code/Code_group3/Home.Designer.cs
+++ Code/Code_group3/Home.Designer.cs
@@ -31,14 +31,15 @@
             this.components = new System.ComponentModel.Container();
             this.btnAlarmHandling = new System.Windows.Forms.Button();
             this.btnMonitoring = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtBoxServerStatus = new System.Windows.Forms.TextBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnServerCon = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnAlarmHandling
@@ -48,10 +49,10 @@
             this.btnAlarmHandling.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnAlarmHandling.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAlarmHandling.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAlarmHandling.Location = new System.Drawing.Point(143, 327);
-            this.btnAlarmHandling.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAlarmHandling.Location = new System.Drawing.Point(191, 402);
+            this.btnAlarmHandling.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnAlarmHandling.Name = "btnAlarmHandling";
-            this.btnAlarmHandling.Size = new System.Drawing.Size(144, 43);
+            this.btnAlarmHandling.Size = new System.Drawing.Size(192, 53);
             this.btnAlarmHandling.TabIndex = 1;
             this.btnAlarmHandling.Text = "Alarm Handling";
             this.btnAlarmHandling.UseVisualStyleBackColor = false;
@@ -64,50 +65,32 @@
             this.btnMonitoring.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnMonitoring.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnMonitoring.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMonitoring.Location = new System.Drawing.Point(437, 327);
-            this.btnMonitoring.Margin = new System.Windows.Forms.Padding(2);
+            this.btnMonitoring.Location = new System.Drawing.Point(583, 402);
+            this.btnMonitoring.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnMonitoring.Name = "btnMonitoring";
-            this.btnMonitoring.Size = new System.Drawing.Size(123, 43);
+            this.btnMonitoring.Size = new System.Drawing.Size(164, 53);
             this.btnMonitoring.TabIndex = 1;
             this.btnMonitoring.Text = "Monitoring";
             this.btnMonitoring.UseVisualStyleBackColor = false;
             this.btnMonitoring.Click += new System.EventHandler(this.btnMonitoring_Click);
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::Code_group3.Properties.Resources.television;
-            this.pictureBox1.Location = new System.Drawing.Point(437, 215);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(123, 107);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 4;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = global::Code_group3.Properties.Resources.bell;
-            this.pictureBox2.Location = new System.Drawing.Point(167, 237);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(96, 71);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 5;
-            this.pictureBox2.TabStop = false;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 517);
+            this.label1.Location = new System.Drawing.Point(16, 636);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(226, 13);
+            this.label1.Size = new System.Drawing.Size(280, 16);
             this.label1.TabIndex = 6;
             this.label1.Text = "Made by: Weihrauch, Fagerlund and Kaupang";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 13);
+            this.label2.Location = new System.Drawing.Point(17, 16);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.Size = new System.Drawing.Size(90, 16);
             this.label2.TabIndex = 7;
             this.label2.Text = "Server Status:";
             // 
@@ -115,9 +98,10 @@
             // 
             this.txtBoxServerStatus.BackColor = System.Drawing.Color.Azure;
             this.txtBoxServerStatus.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtBoxServerStatus.Location = new System.Drawing.Point(83, 13);
+            this.txtBoxServerStatus.Location = new System.Drawing.Point(111, 16);
+            this.txtBoxServerStatus.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtBoxServerStatus.Name = "txtBoxServerStatus";
-            this.txtBoxServerStatus.Size = new System.Drawing.Size(100, 13);
+            this.txtBoxServerStatus.Size = new System.Drawing.Size(133, 15);
             this.txtBoxServerStatus.TabIndex = 8;
             // 
             // timer1
@@ -126,12 +110,45 @@
             this.timer1.Interval = 500;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Code_group3.Properties.Resources.bell;
+            this.pictureBox2.Location = new System.Drawing.Point(223, 292);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(128, 87);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Code_group3.Properties.Resources.television;
+            this.pictureBox1.Location = new System.Drawing.Point(583, 265);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(164, 132);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnServerCon
+            // 
+            this.btnServerCon.Location = new System.Drawing.Point(425, 38);
+            this.btnServerCon.Name = "btnServerCon";
+            this.btnServerCon.Size = new System.Drawing.Size(159, 102);
+            this.btnServerCon.TabIndex = 9;
+            this.btnServerCon.Text = "ServerCon";
+            this.btnServerCon.UseVisualStyleBackColor = true;
+            this.btnServerCon.Click += new System.EventHandler(this.btnServerCon_Click);
+            // 
             // Home
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Azure;
-            this.ClientSize = new System.Drawing.Size(741, 539);
+            this.ClientSize = new System.Drawing.Size(988, 663);
+            this.Controls.Add(this.btnServerCon);
             this.Controls.Add(this.txtBoxServerStatus);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -140,12 +157,12 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnAlarmHandling);
             this.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Home";
             this.Text = "Form1";
             this.TransparencyKey = System.Drawing.Color.White;
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -161,6 +178,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtBoxServerStatus;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnServerCon;
     }
 }
 
